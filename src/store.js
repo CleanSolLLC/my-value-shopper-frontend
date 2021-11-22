e2b0ecdca2a8f9b0b650f81401e0f8cd254c3635
mamark90 <maqmark90@gmail.com>
2021-11-22 17:05:20 -0500

    updated store component

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import categoryReducer from './reducers/categoryReducer'; 
+import categories from './reducers/categoryReducer'; 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   
 const reducer  = combineReducers({
-    categories: categoryReducer
+    categories
   });
 
   let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
