7f1aa107e5fc53381d02c778df4ba95bc0ee68a2
mamark90 <maqmark90@gmail.com>
2021-11-23 13:12:22 -0500

    set up templates for logging in and logging out

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-export default class Signup extends Component {
+export default class SignUp extends Component {
     render() {
-      renturn(
-        <div>
-           <h3>React Signup Component</h3>
-        </div>  
-      )
+        return (
+            <form>
+                <h3>Sign Up</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+
+            </form>
+        );
     }
 }
-
-e
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './containers/App';
+import { BrowserRouter } from "react-router-dom";
+import App from './App';
+import './index.css'
 import store from './store';
 
 
+
 ReactDOM.render(
-  <Provider store = {store}>
-    <App /> 
-  </Provider>,
+  <BrowserRouter>
+    <Provider store = {store}>
+      <App /> 
+    </Provider>
+  </BrowserRouter>,
 
   document.getElementById('root')
-
 );
