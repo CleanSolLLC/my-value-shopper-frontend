76f873718f975d77f0557986610171c29b541526
mamark90 <maqmark90@gmail.com>
2021-11-25 23:48:06 -0500

    created  primary conatiner named main and in the loginUser action createor added additional logic to persist the user in the session

---
+++ src/Main.js
@@ -0,0 +1,62 @@
+import React from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
+
+//used React sign in and sign up template from https://www.positronx.io/build-react-login-sign-up-ui-template-with-bootstrap-4/
+
+function Main() {
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          {/* <Link className="navbar-brand" to={"/sign-in"}>positronX.io</Link> */}
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
+}
+
+export default Main;
+
+  //componentDidMount() {
+    // fetch("http://localhost:3001/api/v1/categories/1/items/3", {
+    //   method: "GET",
+    // }) 
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+
+   // }
+
+//   render() {
+//     return (
+//       <div className="App">
+//          <h3>Build Sign Up & Login UI Template in React</h3>
+//       </div>
+//   )
+//  }
+
+// }
+
+// export default App;
--- src/actions/loginUser.js
+++ src/actions/loginUser.js
@@ -5,8 +5,17 @@ export const setCurrentUser = user =>  {
   }
 }
 
+export const getCurrentUser = user =>  {
+  console.log("GET CURRENT USER")
+  return {
+    type: "GET_CURRENT_USER",
+    user
+  }
+}
+
 export const loginUser = (formData) => dispatch => {
     fetch('http://localhost:3001/api/v1/sessions/login ', {
+      credentials: 'include',
       method: 'POST',
       headers:{
        'Content-type': 'application/json'
@@ -19,6 +28,25 @@ export const loginUser = (formData) => dispatch => {
         alert(user.error)
       } else {
       dispatch(setCurrentUser(user)) 
+      }
+    })
+    .catch(console.log)
+  };
+
+  export const getUser = () => dispatch => {
+    return fetch('http://localhost:3001/api/v1/sessions/get_current_user ', {
+    credentials: 'include',  
+    method: 'GET',
+      headers:{
+       'Content-type': 'application/json'
+      },
+    })
+    .then(resp => resp.json())
+    .then(user => {
+      if (user.error) {
+        alert(user.error)
+      } else {
+      dispatch(getCurrentUser(user)) 
       }
     })
     .catch(console.log)
