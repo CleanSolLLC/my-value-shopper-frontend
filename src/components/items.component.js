4963f040ded13b211b45171d66448bc5d7ae1698
mamark90 <maqmark90@gmail.com>
2021-12-05 15:29:52 -0500

    update items component to create a list of items

--- src/components/items.component.js
+++ src/components/items.component.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
-//import { connect } from 'react-redux';
-//import { loginUser } from '../actions/login'
+import { connect } from 'react-redux';
 
 
 class Items extends Component {
 
     render() {
         return (
-          <div><h1>Items</h1></div>  
-        );
-    }
+          <div><h1>Items</h1>
+            <ul>
+                {this.props.items.map(item => (
+                  <li>{item.product_title}</li>
+                ))}
+            </ul>
+          </div>  
+        )}
+}      
+const mapStateToProps = (state) => {
+  //console.log(state.user.user.items)
+  return {
+    items: state.user.user.items
+  }
 }
 
-
-export default Items;
+export default connect(mapStateToProps)(Items);
