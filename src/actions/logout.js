86c29503b63484b816cc771bd3b3bf73960dd56a
mamark90 <maqmark90@gmail.com>
2021-12-06 13:10:49 -0500

    delete commented out code from logout.js file

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -3,23 +3,3 @@ export const logoutUser = () => {
    type: 'LOGOUT_USER'
  } 
 }
-
-// export const sessionLogout = () => dispatch {
-//   debugger
-//   dispatch(logout())
-//   localStorage.clear();
-// }
-  
-//**************** Do we need to logout on the server ****************************     
-//   return fetch('http://localhost:3001/api/v1/logout',{
-//     //credentials: 'include',  
-//     method: 'DELETE',
-//     })
-// }
-    // .then(resp => resp.json())
-    // .then(data => {
-    //    if(data["session"] === {}) {
-    //      logout(data)
-    //      console.log(data)
-    //    }   
-    // })
